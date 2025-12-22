751d3f95ed7b1e85c123dd7eeb9b5540b1674609
JQS <ingenieriainformatica@megatecnicentro.com>
2025-12-22 12:23:10 -0500

    [ADD], new app read-only sequence bills

---
+++ mega_apps/mega_account_block_sequence_group/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Control de edición de secuencia en facturas",
+    "version": "1.0",
+    "summary": "Permite editar el número/secuencia de facturas/notas solo a un grupo",
+    "website": "https://mega.realnet.com.co/",
+    "sequence": -135,
+    "category": "MegaTecnicentro/InvoiceAccount",
+    "author": "Jorge Alberto Quiroz Sierra",
+    "depends": ["account"],
+    "data": [
+        "security/security.xml",
+        "views/account_move_views.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3",
+}
