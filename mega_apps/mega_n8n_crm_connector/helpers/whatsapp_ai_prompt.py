282bf6f4bd2287225a3b6e4d89981819d9d2c855
JQS <ingenieriainformatica@megatecnicentro.com>
2026-05-29 15:51:43 -0500

    [FIX] initial message data extraction version 1

--- mega_apps/mega_n8n_crm_connector/helpers/whatsapp_ai_prompt.py
+++ mega_apps/mega_n8n_crm_connector/helpers/whatsapp_ai_prompt.py
@@ -1,13 +1,13 @@
 from textwrap import dedent
 
-from .promts.business_context import get_business_context
-from .promts.welcome_rules import get_welcome_rules
-from .promts.capture_rules import get_capture_rules
-from .promts.flow_rules import get_flow_rules
-from .promts.catalog_rules import get_catalog_rules
-from .promts.json_schema import get_json_schema
-from .promts.session_context import get_session_context
-from .promts.tone_rules import get_tone_rules
+from .prompts.business_context import get_business_context
+from .prompts.welcome_rules import get_welcome_rules
+from .prompts.capture_rules import get_capture_rules
+from .prompts.flow_rules import get_flow_rules
+from .prompts.catalog_rules import get_catalog_rules
+from .prompts.json_schema import get_json_schema
+from .prompts.session_context import get_session_context
+from .prompts.tone_rules import get_tone_rules
 
 
 def get_ai_instruction(session, message: str) -> str:
