8fb58b1cb31bbfafc6fc5911a2e4f41a29693bca
JQS <ingenieriainformatica@megatecnicentro.com>
2026-07-16 14:59:30 -0500

    [FIX], Taller 1

--- mega_apps/car_repair_industry/models/res_users.py
+++ mega_apps/car_repair_industry/models/res_users.py
@@ -11,6 +11,7 @@ class ResUsers(models.Model):
             ('advisor', 'Asesor de recepción'),
             ('technician', 'Técnico mecánico'),
             ('road_test', 'Técnico de ruta'),
+            ('quoter', 'Cotizador'),
         ],
         string="Perfil portal taller",
         compute='_compute_car_repair_portal_profile',
@@ -24,6 +25,7 @@ class ResUsers(models.Model):
         advisor_group = self.env.ref('car_repair_industry.group_fleet_repair_portal_advisor', raise_if_not_found=False)
         technician_group = self.env.ref('car_repair_industry.group_fleet_repair_portal_technician', raise_if_not_found=False)
         road_test_group = self.env.ref('car_repair_industry.group_fleet_repair_portal_road_test', raise_if_not_found=False)
+        quoter_group = self.env.ref('car_repair_industry.group_fleet_repair_portal_quoter', raise_if_not_found=False)
         for user in self:
             profile = False
             if advisor_group and advisor_group in user.groups_id:
@@ -32,6 +34,8 @@ class ResUsers(models.Model):
                 profile = 'technician'
             elif road_test_group and road_test_group in user.groups_id:
                 profile = 'road_test'
+            elif quoter_group and quoter_group in user.groups_id:
+                profile = 'quoter'
             user.car_repair_portal_profile = profile
 
     def _inverse_car_repair_portal_profile(self):
@@ -39,6 +43,7 @@ class ResUsers(models.Model):
             'advisor': self.env.ref('car_repair_industry.group_fleet_repair_portal_advisor', raise_if_not_found=False),
             'technician': self.env.ref('car_repair_industry.group_fleet_repair_portal_technician', raise_if_not_found=False),
             'road_test': self.env.ref('car_repair_industry.group_fleet_repair_portal_road_test', raise_if_not_found=False),
+            'quoter': self.env.ref('car_repair_industry.group_fleet_repair_portal_quoter', raise_if_not_found=False),
         }
         portal_groups = profile_groups.values()
         portal_groups = self.env['res.groups'].browse([group.id for group in portal_groups if group])
