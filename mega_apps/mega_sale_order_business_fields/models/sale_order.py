9ea37a8a04e9a06ff567196d4b543f5f1cb62003
JQS <ingenieriainformatica@megatecnicentro.com>
2026-08-18 13:46:15 -0500

    [FIX], by new field mega_description_general, 3

--- mega_apps/mega_sale_order_business_fields/models/sale_order.py
+++ mega_apps/mega_sale_order_business_fields/models/sale_order.py
@@ -10,3 +10,14 @@ class SaleOrder(models.Model):
         copy=True,
         index=True,
     )
+
+    def _prepare_invoice(self):
+        vals = super()._prepare_invoice()
+
+        order = self[:1]  # normalmente es 1
+        if order.mega_descripcion:
+            # Pasar la Descripción de la orden al campo Descripción general (mega_account_move_business_fields)
+            if "mega_descripcion_general" in self.env["account.move"]._fields:
+                vals["mega_descripcion_general"] = order.mega_descripcion
+
+        return vals
